0fa5ea2f8b74651d420f0e7023d831b422b0c69b
Jason Liartis <jason@pop-os.localdomain>
2023-11-22 15:57:48 +0200

    added handling of in memory models

--- src/api_modules/main_module.py
+++ src/api_modules/main_module.py
@@ -1,13 +1,30 @@
+import pickle
+
 import stanza
 
 from src.api_modules.filtering_module import filter_matches
 from src.api_modules.matching_module import find_matches
-from src.utils.settings import in_memory_models, in_memory_terms
+from src.utils.settings import in_memory_models, in_memory_terms, processed_terms_filepaths
 
 
 def find_terms(docs: list[str], language: str = 'en') -> list:
-    nlp = in_memory_models[language]
-    terms = in_memory_terms[language]
+    if language in in_memory_models:
+        nlp = in_memory_models[language]
+        in_memory_models.move_to_end(language)
+    else:
+        nlp = stanza.Pipeline(language, processors='tokenize, lemma, ner')
+        in_memory_models.popitem(last=False)
+        in_memory_models[language] = nlp
+
+    if language in in_memory_terms:
+        terms = in_memory_terms[language]
+        in_memory_terms.move_to_end(language)
+    else:
+        with open(processed_terms_filepaths[language], 'rb') as fp:
+            terms = pickle.load(fp)
+        in_memory_terms.popitem(last=False)
+        in_memory_terms[language] = terms
+
     in_docs = [stanza.Document([], text=d) for d in docs]
     out_docs = nlp(in_docs)
     filtered_matches = []
