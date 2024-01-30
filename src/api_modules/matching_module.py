679befbb2d84463560e97a0b67709b4ce034d535
Jason Liartis <jason@pop-os.localdomain>
2024-01-30 10:06:07 +0200

    fixed how terms are prefixed and matched

--- src/api_modules/matching_module.py
+++ src/api_modules/matching_module.py
@@ -5,21 +5,18 @@ def find_matches(sentence: stanza.models.common.doc.Sentence,
                  prefixed_terms: dict[str: list[str]]
                  ) -> list[tuple[str, int, int]]:
     matches = []
-    sentence_lemmas = [(word.lemma, word.id, word.start_char, word.end_char)
-                       for word in sentence.words]
-    print(sentence_lemmas)
-    for lemma, lemma_id, start_char, end_char in sentence_lemmas:
-        if lemma in prefixed_terms:
-            for lemmatized_term in prefixed_terms[lemma]:
+    for word in sentence.words:
+        if word.lemma in prefixed_terms:
+            for lemmatized_term in prefixed_terms[word.lemma]:
                 term_len = len(lemmatized_term)
                 zipped_term_text = zip(
                     lemmatized_term,
-                    (word.lemma
-                     for word in sentence.words[lemma_id - 1: lemma_id + term_len - 1])
+                    sentence.words[word.id - 1: word.id + term_len - 1]
                 )
-                if all((lemma1 == lemma2 for lemma1, lemma2 in zipped_term_text)):
-                    matches.append((' '.join(lemmatized_term), start_char, end_char))
-                    print('Found term {} from {} to {}'.format(
-                        ' '.join(lemmatized_term), start_char, end_char))
+                if all((term_lemma == sentence_word.lemma
+                        for term_lemma, sentence_word in zipped_term_text)):
+                    matches.append((' '.join(lemmatized_term),
+                                    word.start_char,
+                                    sentence.words[word.id + term_len - 2].end_char))
     print(matches)
     return matches
