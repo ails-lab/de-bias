be2b118c951a637b36267d9c21ec5179d58508e0
Jason Liartis <jason@pop-os.localdomain>
2024-11-20 17:46:19 +0200

    added new rules to german lemmatizer after feedback:

--- model_preprocessing/german_model.py
+++ model_preprocessing/german_model.py
@@ -8,5 +8,18 @@ model = torch.load(os.path.join(STANZA_RESOURCES_DIR, 'de/lemma/gsd_charlm.pt'),
 word_dict, composite_dict = model['dicts']
 
 word_dict['mohren'] = 'mohr'
+word_dict['liliputanerin'] = 'liliputaner'
+word_dict['muselmänner'] = 'muselmann'
+word_dict['üdländerin'] = 'südländer'
+word_dict['brunnenvergifterin'] = 'brunnenvergifter'
+word_dict['brunnenvergifterinnen'] = 'brunnenvergifter'
+word_dict['pygmäinnen'] = 'pygmäin'
+word_dict['halbjüdinnen'] = 'halbjüdin'
+word_dict['mestinzinnen'] = 'mestinzin'
+word_dict['kanaken'] = 'kanake'
+word_dict['hottentotten'] = 'hottentotte'
+word_dict['androgyne'] = 'androgyn'
+word_dict['mongoloide'] = 'mongoloid'
+
 
 torch.save(model, os.path.join(STANZA_RESOURCES_DIR, 'de/lemma/gsd_charlm_customized.pt'))
