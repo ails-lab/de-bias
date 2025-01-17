ff0c1c743ec96a90596b54fc12ff51ee4df6fb38
Jason Liartis <jason@pop-os.localdomain>
2025-01-17 20:37:30 +0200

    modified compound splitter attribution to align with CC-BY-4.0

--- src/utils/comp_split_german.py
+++ src/utils/comp_split_german.py
@@ -1,3 +1,4 @@
+# Original code and license:
 #
 # This work is licensed under the Creative Commons Attribution 4.0 International License.
 # To view a copy of this license, visit http://creativecommons.org/licenses/by/4.0/
@@ -5,6 +6,7 @@
 #
 # Copyright 2020 by repodiac (see https://github.com/repodiac, also for information how to provide attribution to this work)
 #
+# Some modifications have been made
 
 import ahocorasick
 
