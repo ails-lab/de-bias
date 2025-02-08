1f33ce0f869a23c2086f579825b7e8d3a8e9bd0a
Jason Liartis <jason@pop-os.localdomain>
2025-02-08 23:52:21 +0200

    added timeout to llm prompt

--- src/utils/prompt_llm.py
+++ src/utils/prompt_llm.py
@@ -7,13 +7,14 @@ from src.utils.llm_settings import LLM_ENDPOINT, BASE_PAYLOAD, BASE_HEADERS
 
 
 def prompt_llm(prompt: str) -> str:
-    print(prompt)
+    # print(prompt)
     payload = copy(BASE_PAYLOAD)
     payload['prompt'] = prompt
     response = requests.request('POST',
                                 LLM_ENDPOINT,
                                 headers=BASE_HEADERS,
-                                data=json.dumps(payload))
+                                data=json.dumps(payload),
+                                timeout=5)
     response = response.json()
     # print(response['content'])
     return response['content']
