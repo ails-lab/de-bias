ab6211795f897cb9ce7039b14f4f3d5884af52e8
jliartis <jasonliartis@gmail.com>
2025-02-13 23:35:09 +0200

    Update prompt_llm.py

--- src/utils/prompt_llm.py
+++ src/utils/prompt_llm.py
@@ -7,7 +7,7 @@ from src.utils.llm_settings import LLM_ENDPOINT, BASE_PAYLOAD, BASE_HEADERS
 
 
 def prompt_llm(prompt: str) -> str:
-    # print(prompt)
+    print(prompt)
     payload = copy(BASE_PAYLOAD)
     payload['prompt'] = prompt
     response = requests.request('POST',
